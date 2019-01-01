4207855fa5e41b86805372fddbdbd93af323925e
lockeas16 <lockeas16@gmail.com>
2018-12-31 18:03:18 -0600

    0.0.2 turnLeft and turnRight functions coded and tested

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,15 +1,72 @@
-// Rover Object Goes Here
-// ======================
+// rover definition
+var rover = {
+  // default direction: North
+  direction: "N"
+};
 
-// ======================
+
 function turnLeft(rover){
-  console.log("turnLeft was called!");
+  var msg="turnLeft was called!";
+// directions evaluation  
+  switch (rover.direction) {
+    case "N":
+//    now it faces west 
+      rover.direction = "W";
+      break;
+
+    case "W":
+//    now it faces south 
+      rover.direction = "S";
+      break;
+
+    case "S":
+//    now it faces east 
+      rover.direction = "E";
+      break;
+  
+    case "E":
+//    now it faces north 
+      rover.direction = "N";
+      break;
+  
+    default:
+      msg="Invalid direction!";
+    }
+    console.log(msg);
+    return rover.direction;
 }
 
 function turnRight(rover){
-  console.log("turnRight was called!");
+  var msg="turnRight was called!";
+  // directions evaluation  
+  switch (rover.direction) {
+    case "N":
+//    now it faces east 
+      rover.direction = "E";
+      break;
+
+    case "E":
+//    now it faces south 
+      rover.direction = "S";
+      break;
+
+    case "S":
+//    now it faces west
+      rover.direction = "W";
+      break;
+  
+    case "W":
+//    now it faces north 
+      rover.direction = "N";
+      break;
+  
+    default:
+      msg="Invalid direction!";
+    }
+    console.log(msg);
+    return rover.direction;
 }
 
 function moveForward(rover){
-  console.log("moveForward was called")
+  console.log("moveForward was called");
 }
